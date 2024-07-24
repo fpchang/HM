8657b4c07f468ec2d2e1fc08ac8533de30206322
fpchang <hanfengcfp@outlook.com>
2024-07-24 22:37:08 +0800

    new content

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,13 +1,13 @@
 <template>
 	<view class="container">
-		
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+		<createOrder></createOrder>
 	</view>
 </template>
-
+<script setup>
+	import test from './components/test.vue'
+</script>
 <script>
+
 	export default {
 		data() {
 			return {
