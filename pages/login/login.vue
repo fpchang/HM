112d04fc567afeec9708c16f325f9aeadae5a45c
fpchang <hanfengcfp@outlook.com>
2024-08-21 01:57:44 +0800

    111

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -93,13 +93,23 @@
 						uni.showLoading({
 							title: '正在获取验证码'
 						})
-						setTimeout(() => {
-							uni.hideLoading();
-							// 这里此提示会被this.start()方法中的提示覆盖
-							uni.$u.toast('验证码已发送');
-							// 通知验证码组件内部开始倒计时
-							this.$refs.uCode.start();
-						}, 2000);
+						// setTimeout(() => {
+						// 	uni.hideLoading();
+						// 	// 这里此提示会被this.start()方法中的提示覆盖
+						// 	uni.$u.toast('验证码已发送');
+						// 	// 通知验证码组件内部开始倒计时
+						// 	this.$refs.uCode.start();
+						// }, 2000);
+						uniCloud.callFunction({
+							name:"hm-sendSms",
+							data:{
+								smsCode:this.userForm.smsCode,
+								appId:getApp().globalData.systemInfo.appId,
+								templateId:"100"
+							}
+						}).then(res=>{
+							console.log("sendsms value",res)
+						})
 					} else {
 						uni.$u.toast('倒计时结束后再发送');
 					}
@@ -122,7 +132,7 @@
 						name:'hm_login',
 						data:this.userForm
 					}).then(res=>{
-						console.log("login result",res)
+						console.log("login result",res.data)
 					})
 					
 				}).catch(errors => {
---
+++ uniCloud-alipay/cloudfunctions/hm-sendSms/index.js
@@ -0,0 +1,31 @@
+'use strict';
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event);
+	let {appid,mobile,templateId='34'} =event;
+	console.log("9999",templateId);
+	return 111;
+	  try {
+	    const res = await uniCloud.sendSms({
+	      appid: '__UNI__xxxxxxx',
+	      phone: mobile,
+	      templateId: templateId, // 请替换为自己申请的模板id
+	      data: {
+	        name: 'DCloud',
+	        code: '123456',
+	        expMinute: '3',
+	      }
+	    })
+	    // 调用成功，请注意这时不代表发送成功
+	    return res
+	  } catch(err) {
+	    // 调用失败
+	    console.log(err.errCode)
+	    console.log(err.errMsg)
+	    return {
+	      code: err.errCode,
+	      msg: err.errMsg
+	    }
+	  }
+	
+};
