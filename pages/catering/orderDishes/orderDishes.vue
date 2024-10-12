bf53f0c326e386060fecd02c9b96622a65a2a3c4
fpchang <hanfengcfp@outlook.com>
2024-10-13 01:14:13 +0800

    new content

--- pages/catering/orderDishes/orderDishes.vue
+++ pages/catering/orderDishes/orderDishes.vue
@@ -1,5 +1,5 @@
 <template>
-	<view style="max-width:800px;margin:auto;">
+	<view style="max-width:800px;margin:auto;display:flex;flex-direction:column">
 		<view class="header-title" style=""><text>【{{hotel_name}}】用餐预定</text></view>
 		<!-- <uni-card :is-shadow="false">
 			<text class="uni-body">这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
@@ -38,37 +38,55 @@
 			  </uni-forms-item>
 			</uni-forms>
 		</view>
-		<lee-linkage v-if="show == 1" :list="dataList" @onGoodItem="onGoodItem"></lee-linkage>
-		<lee-linkage v-if="show == 2" :list="dataList" @onGoodItem="onGoodItem">
-			<template v-slot:custom="{row}">
-			   
-				<view class="foods-item" v-for="(item,index) in row.foods">
-				  <view class="name-content"> {{item.name}} </view>
-				  <view> 
-					<u-number-box :min="0" integer v-model="item.checkCount">
-					  <input slot="input" style="width: 100px;text-align: center;" class="input"
-						  v-model="item.checkCount"></input>
-				  </u-number-box>
-				  </view>
-				   
+		<view style="flex:1"> 
+
+			<view class="menu-panal">
+				<view class="leftMenu">
+					<scroll-view scroll-x="false" scroll-y="true" show-scrollbar="false" class="scrollView">
+						<view v-for="(item,index) of  menuList" :class="['menuType-item',index==activeLeft?'menuType-active':'']" @click="checkMenu(index)">
+							<text>{{item.name}}</text>
+						</view>
+					</scroll-view>
 				</view>
-			</template>
-		</lee-linkage>
+				<view class="rightMenu">
+					<scroll-view scroll-x="false" scroll-y="true" show-scrollbar="false" class="scrollView" :scroll-into-view="toMenuDetail_id" @scroll="scrollMenuDetail">
+						<view v-for="(item,index) of  menuList" class="menuDetail" :id="'it'+index">
+							<view  v-for="i of item._id['hm-menuDetail']" class="menuDetail-item"> 
+					
+								<view><text>{{i.name}}</text> </view>
+								<uni-icons v-if="!i.checkCount" type="plus-filled" size="24" color="orange" @click="addMenuCount(i)"></uni-icons>
+								<u-number-box v-if="i.checkCount" :min="0" integer v-model="i.checkCount">
+									<input slot="input" style="width: 40px;text-align: center;" class="input"
+										v-model="i.checkCount"></input>
+								</u-number-box>
+							</view>
+							
+						</view>
+					</scroll-view>
+				</view>
+			</view>
+		</view>
+	<view style="height:150px;background:red"></view>
 	</view>
   </template>
   
   <script>
-	import leeLinkage from '../../../components/StarLee-linkage/linkage'
+  import MenuService from '../../../services/MenuService';
+	import leeLinkage from '../../../components/StarLee-linkage/linkage';
   export default {
 		components: {
 			leeLinkage			 
 		},
 		onload(params){
 			console.log("params",params);
+			
 		},
 		data() {
 			return {
+				activeLeft:0,
+				toMenuDetail_id: 'it0',
 				hotel_name:"见山舍",
+				hotel_id:"66f4d677e4ec9dbeca1f8ff9",
 				mealTypeItems:[
 		// 			{
         //   value: "breakfast",
@@ -118,6 +136,8 @@
           ],
         },
       },
+
+	  menuList:[],
 				show: 2,//1默认，2自定义插槽
 				dataList: [{
 						"name": "素菜",
@@ -309,11 +329,109 @@
 			}
 		},
 		computed:{
-			// hotel_id(){
-			// 	return this.$store.state.hotel_id;
-			// }
+			list2() {
+				const _list = this.menuList[this.value]?._id['hm-menuDetail'];
+				return _list ? _list : [];
+			}
+		},
+		created(){
+		this.getMenuList();
 		},
 		methods: {
+			checkMenu(index){
+				console.log(index);
+				this.activeLeft =index;
+				this.toMenuDetail_id = 'it'+index;
+			},
+			scrollMenuDetail(e){
+				console.log(e)
+			},
+		async getMenuList(){
+		let  menuList= [
+            {
+                "_id": {
+                    "_value": "6703f76877f6ba7e271d4b5f",
+                    "hm-menuDetail": [
+                        {
+                            "_id": "67040af760f84340cef243e4",
+                            "isOffer": true,
+                            "mark": "功1",
+                            "menuType_id": "6703f76877f6ba7e271d4b5f",
+                            "name": "土豆丝1",
+                            "price": 281,
+							checkCount:0
+                        },
+                        {
+                            "_id": "67040f701de6951f97ca0e39",
+                            "isOffer": true,
+                            "mark": "13",
+                            "menuType_id": "6703f76877f6ba7e271d4b5f",
+                            "name": "毛毛菜",
+                            "price": 44,
+							checkCount:0
+                        },
+                        {
+                            "_id": "6706b303e4ec9dbeca170c07",
+                            "isOffer": true,
+                            "mark": "",
+                            "menuType_id": "6703f76877f6ba7e271d4b5f",
+                            "name": "红烧肉",
+                            "price": 48,
+							checkCount:0
+                        }
+                    ]
+                },
+                "hotel_id": "66f4d677e4ec9dbeca1f8ff9",
+                "mark": "家长菜备注2",
+                "name": "家长菜2"
+            }]
+			//this.menuList = this.getMenuListFormat(menuList);
+
+			try {
+                uni.showLoading();
+              const res = await   MenuService.getMenuList(this.hotel_id);
+              console.log("菜单列表",res)
+             // store.commit('updateMenuList', res.result.data);
+			  this.menuList = this.getMenuListFormat(res.result.data);
+              uni.hideLoading();
+            } catch (error) {
+              console.error(error);
+              uni.hideLoading();
+                uni.showModal({
+                  content: "系统异常，请稍候再试！",
+                  confirmText: "确认",
+                });
+            }
+		},
+			getMenuListFormat(menuList=[]){
+			try {
+			let arr =	menuList.map(item=>{
+					item._id['hm-menuDetail'].map(it=>{
+						it.checkCount=0;
+						return it;
+					});
+					return item;
+				});
+				return arr
+			} catch (error) {
+			console.error(error)
+				return []
+			}
+					
+			},
+			addMenuCount(itObj){
+			console.log(itObj)
+				this.menuList= this.menuList.map(item=>{
+					item._id['hm-menuDetail'].map(it=>{
+						if(it._id ==itObj._id){
+							it.checkCount =1;
+						}
+						return it;
+					});
+					return item;
+				});
+				//console.log(arr)
+			},
 			onGoodItem(item){
 				console.log(item);
 				uni.showToast({
@@ -330,13 +448,61 @@
 	display:flex;font-size:25px;height:55px;justify-content:center;align-items:center
  }
   .foods-item{
-	height: 40px;
+	
 	display: flex;
 	justify-content: space-between;
 	.name-content{
 	  flex:1;
 	  display:flex;
-	  align-items: center;
+	  flex-direction: column;
+	  padding:8px 0;
+	  font-size: $uni-font-size-base;
+	  .name-content-item{
+		padding:4px 0 ;
+		
+	  }
+	  .price-style{
+		color:#ff0000;
+		font-weight: 400;
+	  }
 	}
   }
+
+
+  .menu-panal{
+	display: flex;
+	.leftMenu{
+		max-width: 200px;
+		min-width: 120px;
+		.menuType-item{
+			height: 80px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			background-color: #eee;
+		};
+		.menuType-active{
+			background-color: #fff;
+		}
+		
+		
+	};
+	.rightMenu{
+		flex: 1;
+		.menuDetail{
+			.menuDetail-item{
+				height: 50px;
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				padding:0 12px;
+			}
+		}
+		
+	}
+	.scrollView{
+		height:calc(100vh - 110px);
+	}
+	
+}
   </style>
